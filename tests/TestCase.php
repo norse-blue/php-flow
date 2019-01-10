42c8862cda8333540c009357583611d75761e50a
Axel Pardemann <axel.pardemann@norse.blue>
2019-01-10 09:59:36 -0600

    Create package and configurations

---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace NorseBlue\Flow\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+/**
+ * Class TestCase
+ *
+ * @package NorseBlue\Flow\Tests
+ */
+abstract class TestCase extends BaseTestCase
+{
+
+}
